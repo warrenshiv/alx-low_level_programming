383aeea5011b1b90eeb7ec6ca2a7c853ae57363e
warrenshiv <warrenshiv@gmail.com>
2023-06-15 09:39:56 -0700

    Modified

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- *main - mainblock
- *Return: 0
- */
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
-}
--- 0x00-hello_world/5-printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- *main - main block
- *return: 0
- */
-int main(void){
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
-}
