fca2de7b12276314bfa59b31507dc0930609d32e
warrenshiv <warrenshiv@gmail.com>
2023-06-15 10:39:05 -0700

    Modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - main block
- * Return: 0
+ *i Return: 0
  */
 int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
