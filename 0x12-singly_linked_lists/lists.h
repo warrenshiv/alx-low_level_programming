f6a24db825f552be92f5f466012b3b93ac90ec32
warrenshiv <warrenshiv@gmail.com>
2023-07-27 12:41:13 -0700

    A function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
 
 #include <stdlib.h>
 
@@ -7,6 +7,10 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Holberton project
  */
 typedef struct list_s
 {
@@ -21,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
